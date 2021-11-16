8e580a5401ad88068928deb2f145e0b0bc71eeaf
Mario Ramirez <marioram743@gmail.com>
2021-11-16 21:20:08 +0100

    Inicio de plantilla de estilos

--- var/cache/dev/twig/e6/e681368f10dfb950436f80fbc81adbbcde5768f276741caa9dcd24d7016f7b51.php
+++ var/cache/dev/twig/e6/e681368f10dfb950436f80fbc81adbbcde5768f276741caa9dcd24d7016f7b51.php
@@ -1190,6 +1190,6 @@ div.sf-toolbar .sf-toolbar-block a:hover {
         display: none !important;
     }
 }
-", "@WebProfiler/Profiler/toolbar.css.twig", "C:\\wamp64\\www\\teader\\website-skeleton\\vendor\\symfony\\web-profiler-bundle\\Resources\\views\\Profiler\\toolbar.css.twig");
+", "@WebProfiler/Profiler/toolbar.css.twig", "C:\\wamp64\\www\\teader\\vendor\\symfony\\web-profiler-bundle\\Resources\\views\\Profiler\\toolbar.css.twig");
     }
 }
